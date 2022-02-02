ce04c2f7606a51836858ee7a8769bbc0e51615af
Caio César Amorim <caiocavalcante063@gmail.com>
2022-02-02 19:18:47 -0300

    finish issue

--- src/CheckoutProgressBar.component.js
+++ src/CheckoutProgressBar.component.js
@@ -4,37 +4,50 @@
  import './CheckoutProgressBar.style';
  
  export class CheckoutProgressBar extends PureComponent {
-    createDot(title, number) {
-      const pin = React.createElement('div', { class: 'dot-progress-pin progress-bar-element' });
-      const pinNumber =  React.createElement('div', { class: 'dot-progress-pin-number progress-bar-element' }, number);
-      const pinTitle = React.createElement('span', { class: 'dot-progress-pin-title progress-bar-element' }, title);
-      const dot = React.createElement('div', { class: 'dot-progress-checkout progress-bar-element' }, [pin, pinNumber, pinTitle])
+    constructor() {
+      super();
+
+      this.state = {
+        progressBar: null,
+        path: null,
+      }
+    }
+
+    createDot(title, number, customClass) {
+      const pin = React.createElement('div', { class: `dot-progress-pin${customClass ? `-${customClass}` : ''}` });
+      const pinNumber =  React.createElement('div', { class: `dot-progress-pin-number${customClass ? `-${customClass}` : ''}` }, number);
+      const pinTitle = React.createElement('span', { class: `dot-progress-pin-title${customClass ? `-${customClass}` : ''}` }, title);
+      const dot = React.createElement('div', { class: `dot-progress-checkout${customClass ? `-${customClass}` : ''}` }, [pin, pinNumber, pinTitle])
 
       return dot;
     }
 
-    createBar() {
-      const bar = React.createElement('div', { class: 'bar-progress-checkout progress-bar-element' });
+    createBar(customClass) {
+      const bar = React.createElement('div', { class: `bar-progress-checkout${customClass ? `-${customClass}` : ''}` });
       return bar
     }
 
 
-    createProgressBarElements() {
+    createProgressBarElements(customClass, progressLimiter) {
       const { stepsNumber, steps } = this.props;
       // if the number of steps is less than 3, progressbar is not useful
       if ( stepsNumber < 3 ) return null;
 
-      let totalSteps = stepsNumber + (stepsNumber - 1);
-
       const stepsArr = [];
+      let totalSteps = stepsNumber + (stepsNumber - 1);
       let innerCounter = 0;
 
        // populating stepsArr
       for (let i = 1; i <= totalSteps; i += 1) {
         if (stepsArr.length % 2 === 0) {
-          stepsArr.push(this.createBar())
+          stepsArr.push(
+            i <= progressLimiter ?  this.createBar(customClass) : this.createBar()
+            )
         } else {
-          stepsArr.push(this.createDot(steps[innerCounter], innerCounter + 1))
+          stepsArr.push(
+            i <= progressLimiter ? this.createDot(steps[innerCounter], (innerCounter + 1), customClass)
+            : this.createDot(steps[innerCounter], (innerCounter + 1))
+            )
           innerCounter += 1;
         }
       }
@@ -42,31 +55,36 @@
       return stepsArr;
     }
 
-    renderProgressBar() {
-      const elementsArr = this.createProgressBarElements();
-      return React.createElement('div', { class: 'progress-bar-wrapper' }, elementsArr)
+    renderProgressBar(customClass, progressLimiter) {
+      const elementsArr = this.createProgressBarElements(customClass, progressLimiter);
+      const progressBar = React.createElement('div', { class: 'progress-bar-wrapper' }, elementsArr)
+      this.setState({ progressBar })
+    }
+
+    componentDidMount() {
+      this.renderProgressBar();
     }
 
     componentDidUpdate() {
       const { steps } = this.props;
+      const { path } = this.state;
       const transformedSteps = steps.map(step => step.toLowerCase());
-      const path = window.location.pathname.split('checkout/')[1];
-      const progressBarElements = Array.from(document.querySelectorAll('.progress-bar-element'));
+      const pathName = window.location.pathname.split('checkout/')[1];
 
-      const progressLimiter = () => {
-        const limiter = transformedSteps.indexOf(path) + 5;
-
-        for (let i = 0; i >= (limiter - 1); i += 1) {
-          progressBarElements[i].classList.add('filled')
-        }
+      if (path !== pathName) {
+        this.setState({ path: pathName })
+        const pathNameIndex = transformedSteps.indexOf(pathName) === -1 ?
+          (transformedSteps.length * 2) + 1
+          : transformedSteps.indexOf(pathName) + 1;
+        const progressLimiter = pathNameIndex * 2;
+        this.renderProgressBar('filled', progressLimiter);
       }
-
-      progressLimiter();
     }
  
      render() {
+       const { progressBar } = this.state;
          return (
-          this.renderProgressBar()
+         progressBar
          );
      }
  }
